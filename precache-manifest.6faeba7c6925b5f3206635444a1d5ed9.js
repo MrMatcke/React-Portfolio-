504dd6e5f23b9355136d198afc9e61b0e5079a44
Mike Matcke <mr.matcke@gmail.com>
2021-05-06 21:01:16 -0700

    Updates

---
+++ precache-manifest.6faeba7c6925b5f3206635444a1d5ed9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "373a0fbd93fa4661bc70",
+    "url": "/React-Portfolio-/static/css/main.618d834a.chunk.css"
+  },
+  {
+    "revision": "373a0fbd93fa4661bc70",
+    "url": "/React-Portfolio-/static/js/main.373a0fbd.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React-Portfolio-/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "fdd2f69c94e99fba93fb",
+    "url": "/React-Portfolio-/static/js/runtime~main.fdd2f69c.js"
+  },
+  {
+    "revision": "0c83118d29a7b61b31603e6f04d86d9d",
+    "url": "/React-Portfolio-/index.html"
+  }
+];
