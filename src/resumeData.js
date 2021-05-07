b3499fcc3fd6578820f938f8c863e0f78b309559
Mike Matcke <mr.matcke@gmail.com>
2021-05-06 18:01:22 -0700

    updating folders and files

---
+++ src/resumeData.js
@@ -0,0 +1,118 @@
+let resumeData = {
+    "imagebaseurl":"git@github.com:MrMatcke/React-Portfolio-.git",
+    "name": "Mike Matcke",
+    "role": "Content Creater | Technical PM | Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/michael-matcke-926b2755/",
+    "skypeid": "Your skypeid",
+    "roleDescription": " Experienced technical professional in the Digital Space",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/michael-matcke-926b2755/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/MrMatcke",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"instagram",
+          "url":"https://www.instagram.com/madovation/?hl=en",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":" Thanks for checking out my site! I love to learn! My own curiosity and creativity has lead me to discovering different skill sets over the years. Originally, I was a videographer, recording everything and making short movies. Eventually, this lead to me getting hired to film and produce videos for weddings, music videos, segments for news stations and other projects during my weekends. Eventually this lead me to accept a role in Project Managent. Working on digital products at the company I worked for. Since then, I have continued to educate myself on everything Digital! Including working on my Programming skills. I currently work for a Digital Agency where I share my digital knowledge with clients as well as help them execute on their projects. ",
+    "Location":"San Diego, CA USA",
+    "website":"tuffrocking.com",
+    "education":[
+      {
+        "UniversityName":"UCSD FUll Stack Web Development Bootcamp",
+        "specialization":"React, Mongo, Express, Node, Bootstrap",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2021",
+       
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Digital Operative",
+        "specialization":"Senior Project Manager - eCommerce",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Manage 4 Ecommerce Dev Projects"
+      },
+      {
+        "CompanyName":"American Addiction Center",
+        "specialization":"Technical PM/Scrum Master",
+        "MonthOfLeaving":"February",
+        "YearOfLeaving":"2020",
+        "Achievements":"Managed 3 Digital Marketing Product Teams"
+      },
+      {
+        "CompanyName":"HD Supply FM",
+        "specialization":"Ecommerce, Master Data Project Manager",
+        "MonthOfLeaving":"August",
+        "YearOfLeaving":"2018",
+        "Achievements":"7 years as a PM, Launching Multiple Digital Projects like Related Products and Direct Ship Program"
+      },
+
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Agile Scrum Methodoligies"
+      },
+      {
+        "skillname":"Project Management"
+      },
+      {
+        "skillname":"Programming (HTML5, Bootstrap, Js, React, Node, Mongo) "
+      },
+      {
+        "skillname":"Digital Content Creation (Videography, Photography,)"
+      },
+      {
+        "skillname":"Digital Marketing"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/g7.jpg"
+        
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/g8.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/g9.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/g10.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Mike is the definition of a true team player whose dedication to the success of our product was evident on a daily basis. He kept detailed documentation of our projects, from requirements to deliverables, and made sure I always had what I needed to succeed. Mike would be an asset to any team – he brings a positive energy and attitude to the office and gets along very well with stakeholders. I hope to work with him again.",
+        "name":"Emily, UX Product Designer"
+      },
+      {
+        "description":"It was a true pleasure to work with Mike. He is an extremely hard worker and brought a lot of experience and knowledge to his position. Mike was able to hit the ground running with this position and was able to provide the very necessary support and guidance for our team. He excelled in being both a support system and an advocate when it came to team members and their needs.",
+        "name":"Kindra, Senior Web Content Editor"
+      },
+      {
+        "description":"Mike was my internship mentor for a web enhancement project that we implemented, which was estimated to increase online revenue by $1.6 million in the first year launched. The project had stakeholders under multiple departments and many complex user requirements. Mike was very strong at identifying who needed to be involved throughout the duration of the project and which requirements to prioritize, all under a timely go-live date. What impressed me most about him was his understanding of the business as a whole, which is key for a successful Project Manager involved in cross-functional projects.",
+        "name":"Laura, Client Success Manager"
+      }
+    ]
+  }
+  
+  export default resumeData
