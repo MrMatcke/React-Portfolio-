f978131f0778d82ed5600f0e8061919b7f5651ad
Mike Matcke <mr.matcke@gmail.com>
2021-05-06 20:56:13 -0700

    updating folder

---
+++ build/precache-manifest.b9bc10c4ea85d643f601237f161bd3ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "513a7592b9370409bea6",
+    "url": "/static/css/main.618d834a.chunk.css"
+  },
+  {
+    "revision": "513a7592b9370409bea6",
+    "url": "/static/js/main.513a7592.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d135c4a46bc585807ad20d5a51fd34f5",
+    "url": "/index.html"
+  }
+];
