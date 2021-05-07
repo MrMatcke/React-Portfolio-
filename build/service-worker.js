16f3f4a9c17c7368d0e6804ec7420a9c67d2d2f3
Mike Matcke <mr.matcke@gmail.com>
2021-05-06 21:02:00 -0700

    github deploy issue

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9bc10c4ea85d643f601237f161bd3ad.js"
+  "/React-Portfolio-/precache-manifest.6faeba7c6925b5f3206635444a1d5ed9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/React-Portfolio-/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
